f94a748ef5f7d0ee452d0a91e325885d0968a390
Robert Groves <rwgdev@gmail.com>
2020-03-23 18:02:07 -0500

    Implement schedule generation.
    Uses a simple round-robin algorithm.

--- plump-robin.js
+++ plump-robin.js
@@ -71,10 +71,10 @@ function ScheduleGenerator({ contestants }) {
       return;
     }
 
-    setScheduleDetails([
-      [["C1 vs C14"], ["C2 vs C13"]],
-      [["C1 vs C13"], ["C14 vs C12"]]
-    ]);
+    const scheduler = roundRobinScheduler(contestants);
+    const roundData = scheduler.generateRounds();
+
+    setScheduleDetails(roundData);
   }
 
   return (
@@ -109,3 +109,76 @@ function App() {
 const domContainer = document.querySelector(appContainerId);
 const app = React.createElement(App);
 ReactDOM.render(app, domContainer);
+
+function roundRobinScheduler(competitors) {
+  console.log("x1", competitors);
+
+  const rounds = competitors.length - 1;
+  const firstRotationIdx = 1;
+  const lastRotationIdx = competitors.length - 1;
+
+  // Create helper function to loop through the competitor indicies.
+  const rotatingIndex = ((first, last) => {
+    let i = first - 1;
+
+    // The skipBack option will back the index up by 1.
+    return (skipBack = false) => {
+      skipBack ? i-- : i++;
+
+      if (i > last) {
+        i = first;
+      }
+
+      return i;
+    };
+  })(firstRotationIdx, lastRotationIdx);
+
+  function generateRounds() {
+    const schedule = [];
+
+    for (let curRound = 0; curRound < rounds; curRound++) {
+      const round = [];
+      const home = [];
+      const away = [];
+
+      // Keep the 1st contestant in a fixed position.
+      home.push(0);
+
+      // Fill in the home competitor indicies using a rotating index.
+      for (let i = 0; i < competitors.length / 2; i++) {
+        if (i > 0) {
+          home[i] = rotatingIndex();
+        }
+      }
+
+      // Fill in the away competitor indicies using a rotating index.
+      for (let i = competitors.length / 2 - 1; i > -1; i--) {
+        away[i] = rotatingIndex();
+      }
+
+      // Skip the index back one.
+      rotatingIndex(true);
+
+      // Generate the matches for the current round.
+      for (let i = 0; i < home.length; i++) {
+        const homeIdx = home[i];
+        const awayIdx = away[i];
+        const homeCompetitor = competitors[homeIdx];
+        const awayCompetitor = competitors[awayIdx];
+        const match = [`${homeCompetitor}, ${awayCompetitor}`];
+
+        round.push(match);
+      }
+
+      schedule.push(round);
+    }
+
+    console.log(schedule);
+
+    return schedule;
+  }
+
+  return {
+    generateRounds
+  };
+}
