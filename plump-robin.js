886abe439d6526a50e5d11b50f471600257f4cbb
Robert Groves <rwgdev@gmail.com>
2020-03-25 23:04:14 -0500

    Add feedback for ContestantCollector interaction.

--- plump-robin.js
+++ plump-robin.js
@@ -25,16 +25,22 @@ function ContestantList({ contestants }) {
 function ContestantCollector({ onAdd }) {
   const [name, setName] = React.useState("");
   const [feedback, setFeedback] = React.useState("");
+  const [feedbackDisplay, setFeedbackDisplay] = React.useState("d-none");
 
   function addCompetitor() {
     if (name && name.length > 0) {
       if (onAdd(name)) {
         setName("");
         setFeedback("");
+        setFeedbackDisplay("d-none");
         document.getElementById("add-competitor-input").focus();
       } else {
+        setFeedbackDisplay("d-block");
         setFeedback(`Contestant ${name} already in list.`);
       }
+    } else {
+      setFeedbackDisplay("d-block");
+      setFeedback(`Please enter the name of a competitor.`);
     }
   }
 
@@ -82,7 +88,11 @@ function ContestantCollector({ onAdd }) {
             </button>
           </div>
         </div>
-        <div>{feedback}</div>
+        <div className={feedbackDisplay}>
+          <div className="pl-2 mb-2 alert alert-danger" role="alert">
+            {feedback}
+          </div>
+        </div>
       </form>
     </div>
   );
